53b328392491da1f814b7402950e8c13c21724ae
Micah Cooper <jmicah@library.tamu.edu>
2016-03-03 18:37:23 -0600

    Removed unused imports.

--- src/main/java/com/planets/game/model/repo/impl/PlayerRepoImpl.java
+++ src/main/java/com/planets/game/model/repo/impl/PlayerRepoImpl.java
@@ -2,8 +2,6 @@ package com.planets.game.model.repo.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
-import com.planets.app.model.AppUser;
-import com.planets.app.model.repo.AppUserRepo;
 import com.planets.game.enums.RaceType;
 import com.planets.game.model.Player;
 import com.planets.game.model.repo.PlayerRepo;
